c74199fbb155e49f282e7e1e6acca2b345e9996f
Andrii Pereverziev <a.pereverziev@enamine.net>
2022-09-06 14:23:18 -0700

    constants

--- core/App.php
+++ core/App.php
@@ -7,6 +7,7 @@ class App {
 
     public function __construct()
     {
+        self::init();
         self::$app = Registry::getInstance();
         $this->getParams();
     }
@@ -19,5 +20,23 @@ class App {
                 self::$app->setProperty($k, $v);
             }
         }
+    }
+
+    protected static function init() {
+        define('PUBLIC', __DIR__ . '/../../../../public');
+        define('ROOT', dirname(__DIR__ . '/../../../../..'));
+        define("APP", ROOT . "/app");
+        define("CORE", dirname(__DIR__) . "/core");
+        define("HELPERS", ROOT . "/helpers");
+        define("CACHE", ROOT . '/tmp/cache');
+        define("LOGS", ROOT . '/tmp/logs');
+        define("CONFIG", ROOT . '/config');
+        define("LAYOUT", "default");
+
+        include CORE . '/functions.php';
+
+        define("PATH", siteUrl());
+        define("ADMIN", PATH . '/admin');
+        define("NO_IMAGE", 'uploads/no_image.jpg');
     }
 }
