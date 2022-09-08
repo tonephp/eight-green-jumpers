3241a676f045fae52fc5554c32e25862e4642251
Andrii Pereverziev <a.pereverziev@enamine.net>
2022-09-08 11:42:54 -0700

    Router

---
+++ app/controllers/MainController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\controllers;
+
+use core\Controller;
+
+class MainController extends Controller
+{
+
+    public function indexAction()
+    {
+        echo __METHOD__;
+    }
+
+}
---
+++ app/controllers/admin/MainController.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace app\controllers\admin;
+
+
+class MainController
+{
+
+    public function indexAction()
+    {
+        echo '<h1>ADMIN AREA</h1>';
+    }
+
+}
