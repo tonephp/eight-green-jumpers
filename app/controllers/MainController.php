88fcd902fb5554ca432ea3ef6e18bbc2532d6fe4
Andrii Pereverziev <a.pereverziev@enamine.net>
2022-09-10 01:19:06 -0700

    Model. Db

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -10,7 +10,8 @@ class MainController extends Controller
     public function indexAction()
     {
         $this->layout = 'ishop';
-        $names = ['John', 'Dave', 'Katy'];
+
+        $names = $this->model->get_names();
         $this->setMeta('Main page', 'Description', 'Keywords');
         $this->set(compact('names'));
     }
---
+++ config/config_db.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'dsn' => 'mysql:host=db;dbname=database;charset=utf8',
+    'user' => 'user',
+    'password' => 'pass',
+];
